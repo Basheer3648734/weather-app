baf049b50a6f76d9a8a06e27a5f6fc7577994468
Basheer3648734 <bk3648734@gmail.com>
2019-12-24 19:26:36 +0530

    added new interface

--- public/js/app.js
+++ public/js/app.js
@@ -5,22 +5,34 @@ const weatherform = document.querySelector('form');
 const search = document.querySelector('input');
 const messageone = document.querySelector('#msg-1');
 const messagetwo = document.querySelector('#msg-2');
-// messageone.textContent = 'from javascript'
+const button = document.querySelector('button');
 weatherform.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     messageone.textContent = "loading..."
     messagetwo.textContent = ''
     fetch("/weather?address=" + location).then(res => {
+
+
+        button.addEventListener('click', () => {
+            messageone.className = '';
+            messagetwo.className = '';
+        })
+
+
+
+
         res.json().then((data) => {
             if (data.error) {
                 messageone.textContent = ''
                 messagetwo.textContent = data.error;
-
+                messagetwo.className = 'error';
             }
             else {
+
                 messageone.textContent = data.location
                 messagetwo.textContent = data.forecast
+                messagetwo.className = 'success';
             }
         })
     })
